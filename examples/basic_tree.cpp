335c8782e6791486b86d9f0fb3c1a439991c5735
Penguin5681 <pranavsinha922@gmail.com>
2026-03-21 23:53:28 +0530

    define basic_tree in the cmake file

---
+++ examples/basic_tree.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+#include "vis/Vis.hpp"
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "This is an example for the tree render using the lucid cpp library";
+    return 0;
+}
